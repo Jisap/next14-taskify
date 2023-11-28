c5c2714ef8e3bae0a3ade0feb8af8561da67d5b7
Jorge Matos <jisapjisap@gmail.com>
2023-11-28 17:33:25 +0100

    marketing

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,12 +1,28 @@
 import { Button } from '@/components/ui/button'
 import { Medal } from 'lucide-react'
 import Link from 'next/link'
+import localFont from 'next/font/local'
+import { cn } from '@/lib/utils'
+import { Poppins } from 'next/font/google'
 
+const headingFont = localFont({
+  src: "../../public/fonts/font.woff2"
+});
+
+const textFont = Poppins({
+  subsets: ["latin"],
+  weight: [
+    "100", "200", "300", "400", "500", "600", "700", "800", "900"
+  ]
+})
 
 const MarketingPage = () => {
   return (
     <div className='flex items-center justify-center flex-col'>
-      <div className='flex items-center justify-center flex-col'>
+      <div className={cn(
+        'flex items-center justify-center flex-col',
+        headingFont.className,
+      )}>
         <div className='mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase'>
           <Medal className='h-6 w-6 mr-2'/>
           Nº 1 task managment
@@ -19,7 +35,10 @@ const MarketingPage = () => {
           work forward.
         </div>
       </div>
-      <div className='text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto'>
+      <div className={cn(
+        'text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto',
+        textFont.className
+      )}>
         Collaborate, manage projects, and reach new productivity peaks. From
         high rises to the home office, the way your team works is unique - accomplish 
         it all with Taskify
