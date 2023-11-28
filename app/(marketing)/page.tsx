7bd67a220e463138f43c6baa57b841c41e0ab95e
Jorge Matos <jisapjisap@gmail.com>
2023-11-28 17:16:35 +0100

    marketing

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,4 +1,6 @@
+import { Button } from '@/components/ui/button'
 import { Medal } from 'lucide-react'
+import Link from 'next/link'
 
 
 const MarketingPage = () => {
@@ -22,6 +24,11 @@ const MarketingPage = () => {
         high rises to the home office, the way your team works is unique - accomplish 
         it all with Taskify
       </div>
+      <Button className='mt-6' size="lg" asChild>
+        <Link href="/sign-up">
+          Get Taskify for free
+        </Link>
+      </Button>
     </div>
   )
 }
