3edb5bf4bf14503752c531acf7c5bba6ffb654f2
Jorge Matos <jisapjisap@gmail.com>
2023-11-29 18:04:47 +0100

    Footer

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Navbar } from './_components/navbar'
+import { Footer } from './_components/footer'
 
 const MarketingLayout = ({children}:{children:React.ReactNode}) => {
   return (
@@ -8,7 +9,7 @@ const MarketingLayout = ({children}:{children:React.ReactNode}) => {
       <main className='pt-40 pb-20 bg-slate-100'>
         {children}
       </main>
-      {/* Footer */}
+      <Footer />
     </div>
   )
 }
