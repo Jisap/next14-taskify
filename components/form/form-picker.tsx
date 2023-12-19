611aa692da96c6400e9328a68a550354a0495609
Jorge Matos <jisapjisap@gmail.com>
2023-12-19 20:24:19 +0100

    form-picker

--- components/form/form-picker.tsx
+++ components/form/form-picker.tsx
@@ -1,6 +1,9 @@
 "use client"
 
 import { unsplash } from "@/lib/unsplash";
+import { cn } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useFormStatus } from "react-dom";
 
@@ -31,17 +34,53 @@ export const FormPicker = ({id, errors}:FormPickerProps) => {
         if(result && result.response){
           const newImages = (result.response as Array<Record<string, any>>);
           setImages(newImages)
+        }else{
+          console.error("Failed to get images from Unsplash")
         }
+
       } catch (error) {
         console.log(error);
         setImages([]);
+      }finally{
+        setIsLoading(false);
       }
-    }
-  },[])
+    };
+
+    fetchImages();
+  },[]);
+
+  if(isLoading){
+    return(
+      <div className="p-6 flex items-center justify-center">
+        <Loader2  className="w-6 h-6 text-sky-700 animate-spin"/>
+      </div>  
+    )
+  }
 
   return (
-    <div>
-      form-picker
+    <div className="relative">
+      <div className="grid grid-cols-3 gap-2 mb-2">
+        {images.map((image) => (
+          <div
+            key={image.id} 
+            className={cn(
+              "cursor-pointer relative aspect-video group hover:opacity-75 transition bg-muted",
+              pending && "opacity-50 hover:opacity-50 cursor-auto"
+            )}
+            onClick={() => {
+              if(pending) return;
+              setSelectedImageId(image.id)
+            }}
+            >
+              <Image 
+                src={image.urls.thumb}
+                fill
+                alt="Unsplash image"
+                className="object-cover rounded-sm"
+              />
+          </div>  
+        ))}
+      </div>
     </div>
   )
 }
