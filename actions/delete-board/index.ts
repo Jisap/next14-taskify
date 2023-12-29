5730b5e11ee06c5c2817c7f71cc17926dabba371
Jorge Matos <jisapjisap@gmail.com>
2023-12-29 16:11:27 +0100

    board-options

---
+++ actions/delete-board/index.ts
@@ -0,0 +1,45 @@
+"use server";
+
+import { auth } from "@clerk/nextjs";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+import { createSafeAction } from "@/lib/create-safe-action";
+
+import { DeleteBoard } from "./schema";
+import { InputType, ReturnType } from "./type";
+
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  const { id } = data;
+  let board;
+
+  try {
+    board = await db.board.delete({   // Borra en bd el board según id y orgId
+      where: {
+        id,
+        orgId,
+      },
+    });
+    
+  } catch (error) {
+    return {
+      error: "Failed to delete."
+    }
+  }
+
+  revalidatePath(`/organization/${orgId}`);
+  redirect(`/organization/${orgId}`);
+};
+
+export const deleteBoard = createSafeAction(DeleteBoard, handler);
--- app/(platform)/(dashboard)/board/[boardId]/_components/board-options.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-options.tsx
@@ -8,12 +8,29 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover"
 import { MoreHorizontal, X } from "lucide-react";
+import { deleteBoard } from "@/actions/delete-board";
+import { useAction } from "@/hooks/use-action";
+import { toast } from "sonner";
+
 
 interface BoardOptionsProps {
   id: string;
 };
 
 export const BoardOptions = ({ id }: BoardOptionsProps) => {
+  
+  const { execute, isLoading } = useAction(deleteBoard, {
+    onError: (error) => {
+      toast.error(error);
+    }
+  });
+
+  const onDelete = () => {
+    execute({ id });        // Llama a la action deleteBoard cargada previamente con el hook
+  };                              
+
+
+  
   return (
     <Popover>
       <PopoverTrigger asChild>
@@ -41,6 +58,14 @@ export const BoardOptions = ({ id }: BoardOptionsProps) => {
             <X className="h-4 w-4"/>
           </Button>
         </PopoverClose>
+        <Button
+          variant="ghost"
+          onClick={onDelete}
+          disabled={isLoading}
+          className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm"
+        >
+          Delete this board
+        </Button>
       </PopoverContent>
     </Popover>  
   )
