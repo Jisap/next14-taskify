7f41cdd96eee6016be7bfcc234a8f754d599b837
Jorge Matos <jisapjisap@gmail.com>
2024-01-02 19:06:58 +0100

    listHeader

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -1,8 +1,11 @@
 "use client"
 
+import { updateList } from "@/actions/update-list"
 import { FormInput } from "@/components/form/form-input"
+import { useAction } from "@/hooks/use-action"
 import { List } from "@prisma/client"
 import { ElementRef, useRef, useState } from "react"
+import { toast } from "sonner"
 import { useEventListener } from "usehooks-ts"
 
 interface ListHeaderProps {
@@ -34,9 +37,34 @@ const ListHeader = ({ data }: ListHeaderProps) => {
     }
   };
 
-  useEventListener("keydown", onKeyDown)
+  useEventListener("keydown", onKeyDown);
 
-  const handleSubmit = () => {}
+  const { execute } = useAction(updateList, {
+    onSuccess: (data) => {
+      toast.success(`Renamed to "${data.title}"`);
+      setTitle(data.title);
+      disableEditing();
+    },
+    onError: (error) => {
+      toast.error(error);
+    }
+  });
+
+  const handleSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    const id = formData.get("id") as string;
+    const boardId = formData.get("boardId") as string;
+
+    if (title === data.title) {
+      return disableEditing();
+    }
+
+    execute({
+      title,
+      id,
+      boardId,
+    });
+  }
 
   return (
     <div className="pt-2 px-2 text-sm font-semibold flex justify-between items-start gap-x-2">
@@ -57,6 +85,7 @@ const ListHeader = ({ data }: ListHeaderProps) => {
               defaultValue={title}
               className="text-sm px-[7px] py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition truncate bg-transparent focus:bg-white"
             />
+            <button type="submit" hidden />
           </form>  
         ) : (
           <div 
