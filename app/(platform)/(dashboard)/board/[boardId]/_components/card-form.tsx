2b6a43eff14d24c758e97763d7c6742e698ec7f1
Jorge Matos <jisapjisap@gmail.com>
2024-01-06 12:08:40 +0100

    card-form

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
@@ -1,7 +1,9 @@
 "use client"
 
+import { FormSubmit } from "@/components/form/form-submit";
+import { FormTextarea } from "@/components/form/form-textarea";
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
+import { Plus, X } from "lucide-react";
 import { forwardRef } from "react";
 
 interface CardFormProps {
@@ -17,11 +19,40 @@ export const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(({
   disableEditing,
   isEditing,
 }, ref) => {
+
+  if(isEditing){
+    return (
+      <form className="m-1 py-0.5 px-1 space-y-4">
+        <FormTextarea
+          id="title"
+          onKeyDown={()=>{}}
+          ref={ref}
+          placeholder="Enter a title for this card..."
+        />
+        <input
+          hidden
+          id="listId"
+          name="listId"
+          value={listId}
+        />
+        <div className="flex items-center gap-x-1">
+          <FormSubmit>
+            Add card
+          </FormSubmit>
+          <Button onClick={disableEditing} size="sm" variant="ghost">
+            <X className="h-5 w-5" />
+          </Button>
+        </div>
+      </form>
+    )
+  }
+
   return(
+
     
     <div className="pt-2 px-2">
       <Button
-        onClick={enableEditing}
+        onClick={enableEditing} // isEditing=true -> focus FormTextArea
         className="h-auto px-2 py-1.5 w-full justify-start text-muted-foreground text-sm"
         size="sm"
         variant="ghost"
