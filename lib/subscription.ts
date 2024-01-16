02196808469a88127fd5717d5e04c93237673a66
Jorge Matos <jisapjisap@gmail.com>
2024-01-16 17:11:53 +0100

    subscription

--- lib/subscription.ts
+++ lib/subscription.ts
@@ -2,7 +2,7 @@ import { auth } from "@clerk/nextjs";
 
 import { db } from "@/lib/db";
 
-const DAY_IN_MS = 86_400_000;
+const DAY_IN_MS = 86_400_000;                                       // Cantidad de milisegundos en un día (24 horas).
 
 export const checkSubscription = async () => {
   const { orgId } = auth();
@@ -11,25 +11,25 @@ export const checkSubscription = async () => {
     return false;
   }
 
-  const orgSubscription = await db.orgSubscription.findUnique({
-    where: {
+  const orgSubscription = await db.orgSubscription.findUnique({     // Busca una suscripción de la organización en la base de datos(db.orgSubscription) 
+    where: {                                                        // basada en orgId.
       orgId,
     },
-    select: {
-      stripeSubscriptionId: true,
-      stripeCurrentPeriodEnd: true,
-      stripeCustomerId: true,
-      stripePriceId: true,
+    select: {                                                       // Selecciona los campos que se desean mostrar
+      stripeSubscriptionId: true,                                   
+      stripeCurrentPeriodEnd: true,                                  
+      stripeCustomerId: true,                                       
+      stripePriceId: true,                                           
     },
   });
 
-  if (!orgSubscription) {
+  if (!orgSubscription) {                                           // Si no hay subscripción devuelve false.  
     return false;
   }
 
-  const isValid =
-    orgSubscription.stripePriceId &&
-    orgSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS > Date.now()
+  const isValid =                                                                 // Verifica la validez de la suscripción                                                      
+    orgSubscription.stripePriceId &&                                              // Para ello aseguramos que haya un stripeId
+    orgSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS > Date.now()   // y que la fecha de finalización + 1 sea mayor  a la fecha actual
 
-  return !!isValid;
+  return !!isValid; // Rdo convertido a boolean
 };
